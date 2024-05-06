9ea97b30665b0c76973d509f8eb49eb461009360
Exciision91 <exciision_gaming@hotmail.com>
2024-05-06 12:21:11 +0200

    Initial commit

---
+++ src/main/java/exciision/lovecraft/LovecraftClient.java
@@ -0,0 +1,10 @@
+package exciision.lovecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LovecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
